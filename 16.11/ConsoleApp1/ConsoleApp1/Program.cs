1275106c27e1d1821e1edc7e35cc4eb18827a482
Opilane TTHK <opilane@tthk.ee>
2022-11-16 11:34:20 +0200

    If ja Else

--- 16.11/ConsoleApp1/ConsoleApp1/Program.cs
+++ 16.11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number !");
+            string number = Console.ReadLine();
+            int numberParsed = Int16.Parse(number);
+
+            // teen siia if ja else loogikaga vastused
+
+
+            Console.ReadKey();
+
+            {
+                if (numberParsed <= 50)
+
+                    Console.WriteLine("See nr on võrdne või väiksem 50-st");
+
+                else
+
+                {
+                    Console.WriteLine("Kõik ülejäänud numbrid.");
+                }
+            }
         }
+
+
+
     }
 }
+
